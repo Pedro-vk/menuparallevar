fee19e20e103d5f4458dcf78951e177cf52027f6
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-05-16 04:11:40 +0200

    Clean firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,10 +3,5 @@ export const environment = {
   firebase: {
     apiKey: 'AIzaSyBJWr4GuNQZxJHb4FokEtV5dsIF2HUc2zM',
     authDomain: 'auth.menuparallevar.com',
-    databaseURL: 'https://llevatelo-a-casa.firebaseio.com',
-    projectId: 'llevatelo-a-casa',
-    storageBucket: 'llevatelo-a-casa.appspot.com',
-    messagingSenderId: '854514941827',
-    appId: '1:854514941827:web:4c8459ad22b5460ac180b6',
   },
 }
