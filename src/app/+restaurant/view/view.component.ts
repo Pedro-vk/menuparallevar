9bc49c8e70e4ebff32efbaa5d58ee1379002cdb4
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-05-17 04:49:14 +0200

    Show opening status on menu view

--- src/app/+restaurant/view/view.component.ts
+++ src/app/+restaurant/view/view.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
-import { Observable } from 'rxjs'
-import { switchMap, map } from 'rxjs/operators'
+import { Observable, combineLatest, interval } from 'rxjs'
+import { switchMap, map, startWith } from 'rxjs/operators'
 
 import { Query, GetRestaurantGQL, Restaurant, Menu } from 'src/app/shared/graphql'
 
@@ -12,6 +12,7 @@ import { Query, GetRestaurantGQL, Restaurant, Menu } from 'src/app/shared/graphq
 })
 export class ViewComponent implements OnInit {
   restaurant$: Observable<Restaurant>
+  status$: Observable<{open: boolean, openAt: number, closeAt: number, openRemaining: string}>
   now = Date.now()
 
   constructor(private route: ActivatedRoute, private getRestaurantGQL: GetRestaurantGQL) { }
@@ -29,6 +30,37 @@ export class ViewComponent implements OnInit {
               .filter(({items}) => items.length),
           },
         }))
+      )
+
+    this.status$ = combineLatest(
+      this.restaurant$,
+      interval(10 * 1000).pipe(startWith(null)),
+      _ => _,
+    )
+      .pipe(
+        map(({schedule: {days, openAt, closeAt}}) => {
+          const h24 = 24 * 60 * 60 * 1000
+          const m1 = 60 * 1000
+          const now = (Date.now() - (new Date().getTimezoneOffset() * m1)) % h24
+          let open = false
+          if (openAt <= closeAt) {
+            open = openAt <= now && now <= closeAt
+          } else {
+            open = openAt <= now || now <= closeAt
+          }
+          let openRemaining: any = (closeAt - now)
+          if (openRemaining < 0) {
+            openRemaining += h24
+          }
+          if (openRemaining < (60 * m1)) {
+            const m = Math.ceil(openRemaining / 60 / 1000)
+            openRemaining = `${m} minuto${m !== 1 ? 's' : ''}`
+          } else {
+            const h = Math.floor(openRemaining / 60 / 60 / 1000)
+            openRemaining = `${h} hora${h !== 1 ? 's' : ''}`
+          }
+          return {openAt, closeAt, open, openRemaining}
+        })
       )
   }
 
