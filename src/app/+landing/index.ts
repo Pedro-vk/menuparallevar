90b683fcb972bc9fd24da642f982151b274671df
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-05-19 23:45:40 +0200

    Create about page

--- src/app/+landing/index.ts
+++ src/app/+landing/index.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router'
 
+import { AboutComponent } from './about/about.component'
 import { HomeComponent } from './home/home.component'
 import { NotFoundComponent } from './not-found/not-found.component'
 
 export const landingsComponents = [
+  AboutComponent,
   HomeComponent,
   NotFoundComponent,
 ]
 
 export const landingRoutes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'sobre-nosotros', component: AboutComponent},
   {path: '404', component: NotFoundComponent},
 ]
